bee2e326c90cbdb6df8ff410336eb580a4ba4d31
ayalgelles <ayal.gelles@simplified-networks.com>
2013-04-29 11:23:35 +0000

    new meteor

--- monkeychat.js
+++ monkeychat.js
@@ -1,6 +1,8 @@
+var Fiber = Npm.require('fibers');
+
 Meteor.methods({
     sendchat: function (data) {
-	console.log(data);
+	console.log('\n\n\n------------ incoming...',data);
 	if (this.isSimulation) {
 	    
 	}
@@ -16,8 +18,11 @@ Meteor.methods({
 		    data.appId = Accounts.loginServiceConfiguration.find().fetch()[0].appId
 		}
 	    }
+
 	    try {
-		chat(data);
+		Fiber(function () {
+		    chat(data);
+		}).run();
 	    }
 	    catch(x) {
 		console.log('exception when sending chat', x);
@@ -35,16 +40,16 @@ if (Meteor.isClient) {
 	Meteor.subscribe("messages");
     });
 
-  Template.loginButtox.events({
-    'click button': function(){
-      Meteor.loginWithFacebook({
-  requestPermissions: ['email','xmpp_login']
-}, function (err) {
-  if (err)
-    Session.set('errorMessage', err.reason || 'Unknown error');
-});
-    }
-  });
+    Template.loginButtox.events({
+	'click button': function(){
+	    Meteor.loginWithFacebook({
+		requestPermissions: ['email','xmpp_login']
+	    }, function (err) {
+		if (err)
+		    Session.set('errorMessage', err.reason || 'Unknown error');
+	    });
+	}
+    });
     Template.chat.messages = function(){
 	return Messages.find({});
     };
@@ -68,99 +73,54 @@ if (Meteor.isClient) {
 
 
 if (Meteor.isServer) {
- 
+    
     var argv = process.argv; 
     
-    var channels = {};
-
-    var myChannel = function(data) {
-	console.log('my channel', data);
-	var cl = channels['channel_' + data.fbid];
-	if (!cl) {
-            var xmppinfo = { jid: '-' + data.fbid + '@chat.facebook.com', 
-			     api_key: data.appId, 
-			     access_token: data.accessToken,
-			     host: 'chat.facebook.com' };
-
-	    console.log('from scratch', xmppinfo);
-	    channels['channel_' + data.fbid] = 
-		cl =
-		new xmpp.Client(xmppinfo); 
-	    
-/*	    cl.on('stanza',
-		  function(stanza) {
-		      if (stanza.is('message') &&
-			  // Important: never reply to errors!
-			  stanza.attrs.type !== 'error') {
-			  var message = stanza.getChild('body') && stanza.getChild('body').getText();
-			  try {
-			      if (message) {
-				  var from = stanza.attrs.from.match(/-(\d+)@/)[1]
-				  var to = stanza.attrs.to.match(/-(\d+)@/)[1]
-				  
-				  console.log('message >>> ', from, to, message);
-				  var data = {msg: message, from: from, to: to};
-				  Fiber(function () {
-				      Messages.insert(data);
-				  }).run();
-			      }
-			  }
-			  catch (x) {
-			      
-			  }
-		      }
-		  });*/
-	    
-	    cl.addListener('error', 
-			   function(e) {
-			       console.error(e);
-			       process.exit(1);                                                                                                                                                       
-			   });
-	    
-	    
-	    cl.ready = function(cb) {
-		if (cl.__ready) {
-		    cb();
-		    return;
-		}
-
-		cl.addListener('online', 
-			       function() { 
-				   console.log('ONLINE!');
-				   cl.__ready = true;
-				   cb();
-			       });
-		
-	    };
-	}
-	else {
-	    console.log('already inited');
-	}
-	
-	return cl;
-	
-
-    };
+    hash = {};
 
     var chat = function(data) {
-	var cl = myChannel(data);
-	cl.ready(function() {
-	    console.log("sending", data.msg, "to", data.to);
-	    cl.send(new xmpp.Element('message', 
-				     { to: '-' + data.to + '@chat.facebook.com',                                                                                                   
+	
+        var xmppinfo = { jid: '-' + data.fbid + '@chat.facebook.com', 
+			 api_key: data.appId, 
+			 access_token: data.accessToken,
+			 host: 'chat.facebook.com' };
+
+	console.log('xmpp info', xmppinfo);
+
+	var cl = hash[data.fbid] || new xmpp.Client(xmppinfo); 
+	hash[data.fbid] = cl;
+
+	cl.addListener('error', 
+		       function(e) {
+			   console.log('ERROR >>>>>>>>>>>>>>>' + e);
+		       });	
+
+
+//	cl.addListener('online',  function() { 
+	setTimeout(function(){
+	    console.log('ONLINE!');
+	    
+	    console.log("sending", data.msg, "to", data.to + '\n\n');
+
+	    try {
+		cl.send(new xmpp.Element('message', 
+				     { to: '-' + data.to + '@chat.facebook.com',
 				       type: 'chat'}).                                                                                                                             
-		    c('body'). 
-		    t(data.msg));    
-    
-    typeof(closetime) !== 'undefined' && clearTimeout(closetime);
-      
-    closetime = setTimeout(function(){
-      console.log('closing connection');
-      cl.end();
-    },5000);
-    
-    
-	});
+			c('body'). 
+			t(data.msg));    
+	    }
+	    catch (x) {
+		console.log('ERROR WHILE SENDING >>>>>>>>>>>>>>>>>>>>>>>', x);
+	    }
+	    finally {
+		closetime = setTimeout(function(){
+		    console.log('------------------ closing connection\n\n');
+		    hash[data.fbid] = null;
+		    cl.end();
+		},5000);
+	    }
+
+	},3000);
     };
     
 
@@ -175,12 +135,12 @@ if (Meteor.isServer) {
 
     Meteor.startup(function () {
 	console.log('startup')
-			Accounts.loginServiceConfiguration.remove({});                                                                                                                                  
-                                                                                                                                                                                        
+	Accounts.loginServiceConfiguration.remove({});                                                                                                                                  
+        
         Accounts.loginServiceConfiguration.insert({                                                                                                                                     
-          service: "facebook",                                                                                                                                                          
-          appId: "453582378051001",                                                                                                                                                     
-          secret: "30377c70afaa8b1af011fa62e3c64277"                                                                                                                                    
+            service: "facebook",                                                                                                                                                          
+            appId: "453582378051001",                                                                                                                                                     
+            secret: "30377c70afaa8b1af011fa62e3c64277"                                                                                                                                    
         }); 
     });
 }
